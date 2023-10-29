61e093335246bd0d9516dcf6f605e70c21e5bcb4
Antonio <abamorim36@gmail.com>
2023-10-29 16:54:05 -0300

    Inserting new transaction and returning

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
+import crypto, { randomUUID } from 'node:crypto'
 import { knex } from './dabase'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app
